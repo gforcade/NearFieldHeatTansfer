745a8b87c3f1d0e566fc4735a335598dca06794c
Gavin Forcade <gforc034@uottawa.ca>
2021-06-21 16:29:13 -0400

    Changed file to simulate on Dyson
    Changed file ouput directory to correct folders.
    Skips simulation if file is already present
    Cleaned up commented code

--- multiWorker_v3.jl
+++ multiWorker_v3.jl
@@ -46,138 +46,112 @@ end
     pdoping_InAs = 3.0*(10.0^24)  #highest is best (proven)
     
 
+    ####running simulations
+    #file name
+    fName = "Rad"*string(trunc(Int,round(Radiator_T)))*"fstGap"*string(trunc(Int,round(d_firstgap*1000)))*"Si"*string(trunc(Int,round(d_Rad*1000)))*"Dop"*string(round(Si_ndoping/1000000,sigdigits=1))*"Gap"*string(trunc(Int,round(d_gap*1000)))*"fsf"*string(trunc(Int,round(d_prot*1000)))*"As"*string(round(prot_quat_x,sigdigits=1))*"Dop"*string(round(prot_ndoping/1000000,sigdigits=1))*"InAs"*string(trunc(Int,round(d_InAs_total*1000)))*"Dop"*string(round(ndoping_InAs/1000000,sigdigits=1))*"Q"*string(trunc(Int,round(d_InAsSbP_base*1000)))*"As"*string(round(quat_x,sigdigits=1))*"Dop"*string(round(pdoping_InAs/1000000,sigdigits=1))*"Sub"*string(trunc(Int,round(d_InAs_sub*1000)))*"Dop"*string(round(pdoping_InAs/1000000,sigdigits=1))*".txt"
+    #only simulate if file not present
+    if !isfile("Total Heat Transfer/"*fName)
+        ####simulations for energy transfer
+        xMinProt = 0.0
+        xMinN = 0.0
+        xMinP = 0.0
+        xMinSub = 0.0
+        divProtCell = 1#ceil(Int,sqrt(d_prot*200))
+        divNcell = 5# ceil(Int,sqrt(d_InAs_total*100))
+        divPcell = 1 #ceil(Int,sqrt(d_InAsSbP_base*100))
+        divSubCell = ceil(Int,d_InAs_sub/5.0)
+        #mboxProter(x) = mboxish(x,divProtCell,d_prot/xMinProt)
+        mboxProt = 0.0#find_zero(mboxProter,2.0,Order5())
+        #mboxNer(x) = mboxish(x,divNcell,d_InAs_total/xMinN)
+        mboxN = 0.0#find_zero(mboxNer,2.0,Order5())
+        #mboxPer(x) = mboxish(x,divPcell,d_InAsSbP_base/xMinP)
+        mboxP = 0.0#find_zero(mboxPer,2.0,Order5())
+        #mboxSuber(x) = mboxish(x,divSubCell,d_InAs_sub/xMinSub)
+        mboxSub = 0.0#find_zero(mboxSuber,2.0,Order5())
+        # Open output file.
+        fileStream = open("Total Heat Transfer/"*fName,"w")
+        # Double check thread initialization.
+        write(fileStream, "Julia initialized with "*string(nthreads())*" threads.\n\n")
+        ## External program settings, internal setting contained in uOttawaSlabStruct.jl
+        enrRng = (0.01, 1.0)
+        ## Begin program execution.
+        write(fileStream, "Execution:\n")
+        # Builds slab structure, generating lVar and lPairs.
+        # Arguments are: temperature of the emitter, background temperature, divisions of NCell
+        stats = @timed (lVar, lPairs) = uOttawaSlabs_v3_1(Radiator_T, 300.0, divProtCell, divNcell, divPcell, divSubCell, d_firstgap, d_Rad, d_gap, d_prot, d_InAs_total, d_InAsSbP_base, d_InAs_sub, Si_ndoping, prot_ndoping, ndoping_InAs, pdoping_InAs, quat_x, prot_quat_x, mboxProt, mboxN, mboxP, mboxSub, xMinProt, xMinN, xMinP, xMinSub)
+        write(fileStream, "Layer structure constructed in " * string(stats.time) * " s.\n")
+        # Storage for photon number computations.
+        htPairs = Array{Float64,1}(undef, size(lPairs)[2])
+        # Compute number of generated photons using heat transfer function.
+        # Switch from flux values achieved through prefactors declared in uOttawaSlabStruct.jl.
+        stats = @timed heatTfr!(lVar, lPairs, enrRng, htPairs)
+        write(fileStream, "Generated excitation profile computed " * string(stats.time) * " s.\n")
+        write(fileStream,string(xMinN)*" "*string(xMinP)*" "*string(xMinSub)*" "*string(xMinProt)*" xMin for mbox meshing. InAs, Q, Substrate, Prot respectively. \n")
+        write(fileStream,string(divNcell)*" "*string(divPcell)*" "*string(divSubCell)*" "*string(divProtCell)*" of slices for InAs, Q, and Substrate, Prot respectively.\n\n")
+        # Write results to file.
+        write(fileStream, "Layer boundary edge (microns)	"*" Excitation Density Rate (W cm-2 microns-1)\n\n")
+        # Location of cell boundary within the slab structure.
+        # Output calculation results.
+        for ind = 1 : length(htPairs) - 1
+            write(fileStream,  string(round((lVar.bdrLoc[lPairs[2,ind]] - lVar.bdrLoc[lPairs[2,1]-1]),sigdigits=4))*" "*string(round(htPairs[ind]/((lVar.bdrLoc[lPairs[2,ind]] - lVar.bdrLoc[lPairs[2,ind]-1])),sigdigits=4)) * " \n")
+        end
+        #gold backing absorption
+        write(fileStream,  string(round((lVar.bdrLoc[lPairs[2,length(htPairs)-1]] - lVar.bdrLoc[lPairs[2,1]-1]),sigdigits=4))*" "*string(round(htPairs[length(htPairs)],sigdigits=4)) * " \n")
+        # Flush file stream.
+        close(fileStream)
 
-    ####simulations for energy transfer
-    xMinProt = 0.0
-    xMinN = 0.0
-    xMinP = 0.0
-    xMinSub = 0.0
-    divProtCell = 1#ceil(Int,sqrt(d_prot*200))
-    divNcell = 1# ceil(Int,sqrt(d_InAs_total*100))
-    divPcell = 1 #ceil(Int,sqrt(d_InAsSbP_base*100))
-    divSubCell = 1
-    #mboxProter(x) = mboxish(x,divProtCell,d_prot/xMinProt)
-    mboxProt = 0.0#find_zero(mboxProter,2.0,Order5())
-    #mboxNer(x) = mboxish(x,divNcell,d_InAs_total/xMinN)
-    mboxN = 0.0#find_zero(mboxNer,2.0,Order5())
-    #mboxPer(x) = mboxish(x,divPcell,d_InAsSbP_base/xMinP)
-    mboxP = 0.0#find_zero(mboxPer,2.0,Order5())
-    #mboxSuber(x) = mboxish(x,divSubCell,d_InAs_sub/xMinSub)
-    mboxSub = 0.0#find_zero(mboxSuber,2.0,Order5())
-    # Open output file.
-    #fileStream = open("photonProfile"*string(divNcell)*".txt","w")
-    #if prot_quat_x != 1.0
-    #    fileStream = open("Results/Rad"*string(trunc(Int,round(Radiator_T)))*"fstGap"*string(trunc(Int,round(d_firstgap*1000)))*"Si"*string(trunc(Int,round(d_Rad*1000)))*"Dop"*string(round(Si_ndoping/1000000,sigdigits=1))*"Gap"*string(trunc(Int,round(d_gap*1000)))*"fsf"*string(trunc(Int,round(d_prot*1000)))*"As"*string(round(prot_quat_x,sigdigits=1))*"InAs"*string(trunc(Int,round(d_InAs_total*1000)))*"Dop"*string(round(ndoping_InAs/1000000,sigdigits=1))*"Q"*string(trunc(Int,round(d_InAsSbP_base*1000)))*"As"*string(round(quat_x,sigdigits=1))*"Sub"*string(trunc(Int,round(d_InAs_sub*1000)))*"Dop"*string(round(pdoping_InAs/1000000,sigdigits=1))*".txt","w")
-    #else
-    fileStream = open("Results/Rad"*string(trunc(Int,round(Radiator_T)))*"fstGap"*string(trunc(Int,round(d_firstgap*1000)))*"Si"*string(trunc(Int,round(d_Rad*1000)))*"Dop"*string(round(Si_ndoping/1000000,sigdigits=1))*"Gap"*string(trunc(Int,round(d_gap*1000)))*"fsf"*string(trunc(Int,round(d_prot*1000)))*"As"*string(round(prot_quat_x,sigdigits=1))*"Dop"*string(round(prot_ndoping/1000000,sigdigits=1))*"InAs"*string(trunc(Int,round(d_InAs_total*1000)))*"Dop"*string(round(ndoping_InAs/1000000,sigdigits=1))*"Q"*string(trunc(Int,round(d_InAsSbP_base*1000)))*"As"*string(round(quat_x,sigdigits=1))*"Dop"*string(round(pdoping_InAs/1000000,sigdigits=1))*"Sub"*string(trunc(Int,round(d_InAs_sub*1000)))*"Dop"*string(round(pdoping_InAs/1000000,sigdigits=1))*".txt","w")
-    #end
-    # Double check thread initialization.
-    write(fileStream, "Julia initialized with "*string(nthreads())*" threads.\n\n")
-    # Conversion of photon energy from electron volts to Joules.
-    #const evJ = 1.6021774232052327e-19
-    ## External program settings, internal setting contained in uOttawaSlabStruct.jl
-    enrRng = (0.01, 1.3)
-    ## Begin program execution.
-    write(fileStream, "Execution:\n")
-    # Builds slab structure, generating lVar and lPairs.
-    # Arguments are: temperature of the emitter, background temperature, divisions of NCell
-    # (Resolution of photon generation is thckInAs / divNCell), division of PCell
-    #stats = @timed (lVar, lPairs) = uOttawaSlabs(800.0, 300.0, divNcell, 30)
-    #=if prot_quat_x != 1.0
-        stats = @timed (lVar, lPairs) = uOttawaSlabs_v2_1(Radiator_T, 300.0, divProtCell, divNcell, divPcell, divSubCell, d_gap, d_prot, d_InAs_total, d_InAsSbP_base, d_InAs_sub, ndoping_InAs, pdoping_InAs, quat_x, prot_quat_x, mboxProt, mboxN, mboxP, mboxSub, xMinProt, xMinN, xMinP, xMinSub)
-    else
-        stats = @timed (lVar, lPairs) = uOttawaSlabs_v3_1(Radiator_T, 300.0, divProtCell, divNcell, divPcell, divSubCell, d_gap, d_prot, d_InAs_total, d_InAsSbP_base, d_InAs_sub, prot_ndoping, ndoping_InAs, pdoping_InAs, quat_x, prot_quat_x, mboxProt, mboxN, mboxP, mboxSub, xMinProt, xMinN, xMinP, xMinSub)
-    end=#
-    stats = @timed (lVar, lPairs) = uOttawaSlabs_v3_1(Radiator_T, 300.0, divProtCell, divNcell, divPcell, divSubCell, d_firstgap, d_Rad, d_gap, d_prot, d_InAs_total, d_InAsSbP_base, d_InAs_sub, Si_ndoping, prot_ndoping, ndoping_InAs, pdoping_InAs, quat_x, prot_quat_x, mboxProt, mboxN, mboxP, mboxSub, xMinProt, xMinN, xMinP, xMinSub)
-    write(fileStream, "Layer structure constructed in " * string(stats.time) * " s.\n")
-    # Storage for photon number computations.
-    htPairs = Array{Float64,1}(undef, size(lPairs)[2])
-    # Compute number of generated photons using heat transfer function.
-    # Switch from flux values achieved through prefactors declared in uOttawaSlabStruct.jl.
-    stats = @timed heatTfr!(lVar, lPairs, enrRng, htPairs)
-    write(fileStream, "Generated excitation profile computed " * string(stats.time) * " s.\n")
-    write(fileStream,string(xMinN)*" "*string(xMinP)*" "*string(xMinSub)*" "*string(xMinProt)*" xMin for mbox meshing. InAs, Q, Substrate, Prot respectively. \n")
-    write(fileStream,string(divNcell)*" "*string(divPcell)*" "*string(divSubCell)*" "*string(divProtCell)*" of slices for InAs, Q, and Substrate, Prot respectively.\n\n")
-    # Write results to file.
-    write(fileStream, "Layer boundary edge (microns)	"*" Excitation Density Rate (W cm-2 microns-1)\n\n")
-    # Location of cell boundary within the slab structure.
-    #cellSurfLoc = lVar.bdrLoc[lPairs[2,1]-1]
-    # Output calculation results.
-    for ind = 1 : length(htPairs) - 1
-        #htPairs[ind] = htPairs[ind]*(lVar.bdrLoc[lPairs[2,ind]] - lVar.bdrLoc[lPairs[2,ind-1]])
-        #write(fileStream, string(round(lVar.bdrLoc[lPairs[2,ind]] - cellSurfLoc, sigdigits = 4)) * "," * string(round(/(htPairs[ind], evJ), sigdigits = 4)) * "\n")
-        write(fileStream,  string(round((lVar.bdrLoc[lPairs[2,ind]] - lVar.bdrLoc[lPairs[2,1]-1]),sigdigits=4))*" "*string(round(htPairs[ind]/((lVar.bdrLoc[lPairs[2,ind]] - lVar.bdrLoc[lPairs[2,ind]-1])),sigdigits=4)) * " \n")
+
+
+
+        ###### simulation for photon number
+        xMinProt = 0.0
+        xMinN = 0.0
+        xMinP = 0.0
+        xMinSub = 0.5
+        divProtCell = ceil(Int,sqrt(d_prot*100))
+        divNcell = ceil(Int,sqrt(d_InAs_total*100))
+        divPcell = ceil(Int,sqrt(d_InAsSbP_base*100))
+        divSubCell = 30
+        #mboxProter(x) = mboxish(x,divProtCell,d_prot/xMinProt)
+        mboxProt = 0.0#find_zero(mboxProter,2.0,Order5())
+        #mboxNer(x) = mboxish(x,divNcell,d_InAs_total/xMinN)
+        mboxN = 0.0#find_zero(mboxNer,2.0,Order5())
+        #mboxPer(x) = mboxish(x,divPcell,d_InAsSbP_base/xMinP)
+        mboxP = 0.0#find_zero(mboxPer,2.0,Order5())
+        mboxSuber(x) = mboxish(x,divSubCell,d_InAs_sub/xMinSub)
+        mboxSub = find_zero(mboxSuber,2.0,Order5())
+        # Open output file.
+        fileStream = open("Photon Number/"*fName,"w")
+        #end
+        # Double check thread initialization.
+        write(fileStream, "Julia initialized with "*string(nthreads())*" threads.\n\n")
+        ## External program settings, internal setting contained in uOttawaSlabStruct.jl
+        enrRng = (0.35, 1.0)
+        ## Begin program execution.
+        write(fileStream, "Execution:\n")
+        # Builds slab structure, generating lVar and lPairs.
+        # Arguments are: temperature of the emitter, background temperature, divisions of NCell
+        # (Resolution of photon generation is thckInAs / divNCell), division of PCell
+        stats = @timed (lVar, lPairs) = uOttawaSlabs_v3(Radiator_T, 300.0, divProtCell, divNcell, divPcell, divSubCell, d_firstgap, d_Rad, d_gap, d_prot, d_InAs_total, d_InAsSbP_base, d_InAs_sub, Si_ndoping, prot_ndoping, ndoping_InAs, pdoping_InAs, quat_x, prot_quat_x, mboxProt, mboxN, mboxP, mboxSub, xMinProt, xMinN, xMinP, xMinSub)
+        write(fileStream, "Layer structure constructed in " * string(stats.time) * " s.\n")
+        # Storage for photon number computations.
+        htPairs = Array{Float64,1}(undef, size(lPairs)[2])
+        # Compute number of generated photons using heat transfer function.
+        # Switch from flux values achieved through prefactors declared in uOttawaSlabStruct.jl.
+        stats = @timed heatTfr!(lVar, lPairs, enrRng, htPairs)
+        write(fileStream, "Generated excitation profile computed " * string(stats.time) * " s.\n")
+        write(fileStream,string(xMinN)*" "*string(xMinP)*" "*string(xMinSub)*" "*string(xMinProt)*" xMin for mbox meshing. InAs, Q, Substrate, Prot respectively. \n")
+        write(fileStream,string(divNcell)*" "*string(divPcell)*" "*string(divSubCell)*" "*string(divProtCell)*" of slices for InAs, Q, and Substrate, Prot respectively.\n\n")
+        # Write results to file.
+        write(fileStream, "Depth (microns)	"*" Excitation Density Rate (cm-2 microns-1 s-1)\n\n")
+        # Location of cell boundary within the slab structure.
+        # Output calculation results.
+        for ind = 1 : length(htPairs)
+            write(fileStream, string(round(htPairs[ind]/((lVar.bdrLoc[lPairs[2,ind]] - lVar.bdrLoc[lPairs[2,ind]-1])*evJ),sigdigits=4)) * " \n")
+        end
+        # Flush file stream.
+        close(fileStream)
     end
-    #gold backing absorption
-    write(fileStream,  string(round((lVar.bdrLoc[lPairs[2,length(htPairs)-1]] - lVar.bdrLoc[lPairs[2,1]-1]),sigdigits=4))*" "*string(round(htPairs[length(htPairs)],sigdigits=4)) * " \n")
-    # Flush file stream.
-    close(fileStream)
-
-
-
-
-    ###### simulation for photon number
-    xMinProt = 0.0
-    xMinN = 0.0
-    xMinP = 0.0
-    xMinSub = 0.5
-    divProtCell = ceil(Int,sqrt(d_prot*200))
-    divNcell = ceil(Int,sqrt(d_InAs_total*100))
-    divPcell = ceil(Int,sqrt(d_InAsSbP_base*100))
-    divSubCell = 30
-    #mboxProter(x) = mboxish(x,divProtCell,d_prot/xMinProt)
-    mboxProt = 0.0#find_zero(mboxProter,2.0,Order5())
-    #mboxNer(x) = mboxish(x,divNcell,d_InAs_total/xMinN)
-    mboxN = 0.0#find_zero(mboxNer,2.0,Order5())
-    #mboxPer(x) = mboxish(x,divPcell,d_InAsSbP_base/xMinP)
-    mboxP = 0.0#find_zero(mboxPer,2.0,Order5())
-    mboxSuber(x) = mboxish(x,divSubCell,d_InAs_sub/xMinSub)
-    mboxSub = find_zero(mboxSuber,2.0,Order5())
-    # Open output file.
-    #fileStream = open("photonProfile"*string(divNcell)*".txt","w")
-    #if prot_quat_x != 1.0
-    #    fileStream = open("Results_photonNumber/Rad"*string(trunc(Int,round(Radiator_T)))*"fstGap"*string(trunc(Int,round(d_firstgap*1000)))*"Si"*string(trunc(Int,round(d_Rad*1000)))*"Dop"*string(round(Si_ndoping/1000000,sigdigits=1))*"Gap"*string(trunc(Int,round(d_gap*1000)))*"fsf"*string(trunc(Int,round(d_prot*1000)))*"As"*string(round(prot_quat_x,sigdigits=1))*"InAs"*string(trunc(Int,round(d_InAs_total*1000)))*"Dop"*string(round(ndoping_InAs/1000000,sigdigits=1))*"Q"*string(trunc(Int,round(d_InAsSbP_base*1000)))*"As"*string(round(quat_x,sigdigits=1))*"Sub"*string(trunc(Int,round(d_InAs_sub*1000)))*"Dop"*string(round(pdoping_InAs/1000000,sigdigits=1))*".txt","w")
-    #else
-    fileStream = open("Results_photonNumber/Rad"*string(trunc(Int,round(Radiator_T)))*"fstGap"*string(trunc(Int,round(d_firstgap*1000)))*"Si"*string(trunc(Int,round(d_Rad*1000)))*"Dop"*string(round(Si_ndoping/1000000,sigdigits=1))*"Gap"*string(trunc(Int,round(d_gap*1000)))*"fsf"*string(trunc(Int,round(d_prot*1000)))*"As"*string(round(prot_quat_x,sigdigits=1))*"Dop"*string(round(prot_ndoping/1000000,sigdigits=1))*"InAs"*string(trunc(Int,round(d_InAs_total*1000)))*"Dop"*string(round(ndoping_InAs/1000000,sigdigits=1))*"Q"*string(trunc(Int,round(d_InAsSbP_base*1000)))*"As"*string(round(quat_x,sigdigits=1))*"Dop"*string(round(pdoping_InAs/1000000,sigdigits=1))*"Sub"*string(trunc(Int,round(d_InAs_sub*1000)))*"Dop"*string(round(pdoping_InAs/1000000,sigdigits=1))*".txt","w")
-    #end
-    # Double check thread initialization.
-    write(fileStream, "Julia initialized with "*string(nthreads())*" threads.\n\n")
-    # Conversion of photon energy from electron volts to Joules.
-    #const evJ = 1.6021774232052327e-19
-    ## External program settings, internal setting contained in uOttawaSlabStruct.jl
-    enrRng = (0.35, 1.3)
-    ## Begin program execution.
-    write(fileStream, "Execution:\n")
-    # Builds slab structure, generating lVar and lPairs.
-    # Arguments are: temperature of the emitter, background temperature, divisions of NCell
-    # (Resolution of photon generation is thckInAs / divNCell), division of PCell
-    #stats = @timed (lVar, lPairs) = uOttawaSlabs(800.0, 300.0, divNcell, 30)
-    #if prot_quat_x != 1.0
-    #    stats = @timed (lVar, lPairs) = uOttawaSlabs_v2(Radiator_T, 300.0, divProtCell, divNcell, divPcell, divSubCell, d_gap, d_prot, d_InAs_total, d_InAsSbP_base, d_InAs_sub, ndoping_InAs, pdoping_InAs, quat_x, prot_quat_x, mboxProt, mboxN, mboxP, mboxSub, xMinProt, xMinN, xMinP, xMinSub)
-    #else
-    stats = @timed (lVar, lPairs) = uOttawaSlabs_v3(Radiator_T, 300.0, divProtCell, divNcell, divPcell, divSubCell, d_firstgap, d_Rad, d_gap, d_prot, d_InAs_total, d_InAsSbP_base, d_InAs_sub, Si_ndoping, prot_ndoping, ndoping_InAs, pdoping_InAs, quat_x, prot_quat_x, mboxProt, mboxN, mboxP, mboxSub, xMinProt, xMinN, xMinP, xMinSub)
-    #end
-    write(fileStream, "Layer structure constructed in " * string(stats.time) * " s.\n")
-    # Storage for photon number computations.
-    htPairs = Array{Float64,1}(undef, size(lPairs)[2])
-    # Compute number of generated photons using heat transfer function.
-    # Switch from flux values achieved through prefactors declared in uOttawaSlabStruct.jl.
-    stats = @timed heatTfr!(lVar, lPairs, enrRng, htPairs)
-    write(fileStream, "Generated excitation profile computed " * string(stats.time) * " s.\n")
-    write(fileStream,string(xMinN)*" "*string(xMinP)*" "*string(xMinSub)*" "*string(xMinProt)*" xMin for mbox meshing. InAs, Q, Substrate, Prot respectively. \n")
-    write(fileStream,string(divNcell)*" "*string(divPcell)*" "*string(divSubCell)*" "*string(divProtCell)*" of slices for InAs, Q, and Substrate, Prot respectively.\n\n")
-    # Write results to file.
-    write(fileStream, "Depth (microns)	"*" Excitation Density Rate (cm-2 microns-1 s-1)\n\n")
-    # Location of cell boundary within the slab structure.
-    #cellSurfLoc = lVar.bdrLoc[lPairs[2,1]-1]
-    # Output calculation results.
-    for ind = 1 : length(htPairs)
-        #htPairs[ind] = htPairs[ind]*(lVar.bdrLoc[lPairs[2,ind]] - lVar.bdrLoc[lPairs[2,ind-1]])
-        #write(fileStream, string(round(lVar.bdrLoc[lPairs[2,ind]] - cellSurfLoc, sigdigits = 4)) * "," * string(round(/(htPairs[ind], evJ), sigdigits = 4)) * "\n")
-        write(fileStream, string(round(htPairs[ind]/((lVar.bdrLoc[lPairs[2,ind]] - lVar.bdrLoc[lPairs[2,ind]-1])*evJ),sigdigits=4)) * " \n")
-    end
-    # Flush file stream.
-    close(fileStream)
 end
 
 
@@ -195,6 +169,4 @@ end
 # perform simulations and keep track of how much there is left
 @showprogress pmap(i -> twoParams(i),1:length(sweep1)*length(sweep2))
 
-#@distributed for i = 1:length(sweep1)*length(sweep2)
-#    twoParams(i)
-#end
+
