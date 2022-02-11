7e4e6c50d4ae52cc10cfcfe130cbf4acad041501
Gavin Forcade <gforc034@uottawa.ca>
2022-02-11 11:16:58 -0500

    k_IB for Si
    added imaginary part of refractive index to Si. This is only useful for InGaAs simulations as InAs has to low a bandgap to affect this.
    
    Also made file to simulate heat tranfer between 2 semi-infinite Si plates

---
+++ Si_HeatTransfer.jl
@@ -0,0 +1,123 @@
+## heat transfer between doped silicon slabs
+## This is to validate the optical model  
+
+#directory to code
+savFileDir = pwd()#"/home/gforc034/hs_Sean_02102021_v3"
+
+push!(LOAD_PATH,savFileDir)
+using Base.Threads, ProgressMeter, Roots, Base, Printf
+using FilmDataStructures, FilmUtilities, ResponseModels
+const evJ = 1.6021774232052327e-19
+
+
+# T [K], d [um]
+Si1_T=1000.0
+gap_d=10.0
+Si2_T=300.0
+
+
+
+#changing the parameters from sentaurus units to optical model units
+#dop in cm-3
+Si1_dop = 1.0e20
+Si2_dop = 1.0e20
+
+
+
+####running simulations
+#file name
+fName = "SiRad"#"Rad"*string(trunc(Int,round(Rad_T)))*"fstGap"*string(trunc(Int,round(firstgap_d*1000)))*"Si"*string(trunc(Int,round(Rad_d*1000)))*"Dop"*string(round(Rad_dop/1000000,sigdigits=1))*"Gap"*string(trunc(Int,round(gap_d*1000)))*"fsf"*string(trunc(Int,round(fsf_d*1000)))*"As"*string(round(fsf_As,sigdigits=1))*"Dop"*string(round(fsf_dop/1000000,sigdigits=1))*"InAs"*string(trunc(Int,round(emitter_d*1000)))*"Dop"*string(round(emitter_dop/1000000,sigdigits=1))*"Q"*string(trunc(Int,round(base_d*1000)))*"As"*string(round(base_As,sigdigits=1))*"Dop"*string(round(base_dop/1000000,sigdigits=1))*"Sub"*string(trunc(Int,round(substrate_d*1000)))*"Dop"*string(round(substrate_dop/1000000,sigdigits=1))*".txt"
+#only simulate if file not present
+#if !isfile(savFileDir*"/Photon Number/"*fName)
+####simulations for energy transfer
+
+#temperature of layers
+numLayers = 3
+tmpLst = fill(Si2_T, numLayers)
+tmpLst[1] = Si1_T
+#border Location
+bdrLoc = Vector{Float64}(undef, numLayers - 1) 
+bdrLoc[1] = 0.0
+bdrLoc[2] = gap_d
+#silicon dielectric model parameters
+acpDpt_top = dptDsc(0.045, 0.0)
+dnrDpt_top = dptDsc(0.0456, abs(Si1_dop))
+#for bottom
+acpDpt = dptDsc(0.045, 0.0)
+dnrDpt = dptDsc(0.0456, abs(Si2_dop))
+#calculates silicon model parameters for slab way above
+siModParamsE_top = prmMSi(Si1_T, dnrDpt_top, acpDpt_top)
+# Construct silicon response model.
+siRspE_top(enr) = siRsp(enr, siModParamsE_top)
+siAbsE_top(enr) = imag(siRspE(enr))
+#Si for bottom layer
+siModParamsE = prmMSi(Si2_T, dnrDpt, acpDpt)
+# Construct silicon response model.
+siRspE(enr) = siRsp(enr, siModParamsE)
+siAbsE(enr) = imag(siRspE(enr))
+# Gap response.
+gRsp(enr) = cstRsp(1.0 + im * ^(10.0, -7), enr)
+gAbs(enr) = 0.0
+
+## Generate lists of optical responses and transfer factors.
+optRsp = []
+trfFacs = []  #absorption
+push!(optRsp, siRspE_top, gRsp, siRspE) 
+#air,Si, air, Si, air 
+push!(trfFacs, siAbsE_top, gAbs, siAbsE)
+
+## Set which layers transmission should be calculated for.
+lPairs = Array{Int64,2}(undef, 2, 1) #+1 for gold back-reflector
+lPairs[1, 1] = 1
+lPairs[2, 1] = 3
+lVar = lyrDsc(bdrLoc, tmpLst, optRsp, trfFacs)
+
+enrRng = (6.6e-4, 1.319) #energy interval
+# Builds slab structure, generating lVar and lPairs.
+# Arguments are: temperature of the emitter, background temperature, divisions of NCell
+
+
+
+# Storage for heat transfer computations.
+htPairs = Array{Float64,1}(undef, size(lPairs)[2])
+# Compute heat transfer using heat transfer function.
+stats2 = @timed heatTfr!(lVar, lPairs, enrRng, htPairs)
+# Open output file.
+#=
+fileStream = open(savFileDir*"/Total Heat Transfer/"*fName,"w")
+# Double check thread initialization.
+write(fileStream, "Julia initialized with "*string(nthreads())*" threads.\n\n")
+## External program settings, internal setting contained in uOttawaSlabStruct.jl
+## Begin program execution.
+write(fileStream, "Execution:\n")
+write(fileStream, "Layer structure constructed in " * string(stats1.time) * " s.\n")
+write(fileStream, "Generated excitation profile computed " * string(stats2.time) * " s.\n")
+write(fileStream,string(xMinN)*" "*string(xMinP)*" 0.0 "*string(xMinProt)*" xMin for mbox meshing. Emitter, Base, Substrate, Prot respectively. \n")
+write(fileStream,string(divNcell)*" "*string(divPcell)*" "*string(divSubCell)*" "*string(divProtCell)*" of slices for emitter, base, and Substrate, Prot respectively.\n\n")
+# Write results to file.
+write(fileStream, "Layer boundary edge (microns)	"*" Excitation Density Rate (W cm-2 microns-1)\n\n")
+# Location of cell boundary within the slab structure.
+# Output calculation results.
+for ind = 1 : length(htPairs) - 1
+    write(fileStream,  string(round((lVar.bdrLoc[lPairs[2,ind]] - lVar.bdrLoc[lPairs[2,1]-1]),sigdigits=4))*" "*string(round(htPairs[ind]/((lVar.bdrLoc[lPairs[2,ind]] - lVar.bdrLoc[lPairs[2,ind]-1])),sigdigits=4)) * " \n")
+end
+#gold backing absorption
+write(fileStream,  string(round((lVar.bdrLoc[lPairs[2,length(htPairs)-1]] - lVar.bdrLoc[lPairs[2,1]-1]),sigdigits=4))*" "*string(round(htPairs[length(htPairs)],sigdigits=4)) * " \n")
+# Flush file stream.
+close(fileStream)
+=#
+println("Heattransfer execution time = ",stats2.time)
+println("Total heat transfer (W/m2) = ", @sprintf("%.2E",sum(htPairs)*1e4))
+    
+
+
+   
+#end
+
+
+
+
+
+
+
+
--- siMod.jl
+++ siMod.jl
@@ -4,6 +4,7 @@
 # Physics 44, 353 (1976); doi: 10.1119/1.10194.
 ## Dependencies
 using Roots
+
 # Conversion factors are loaded from call through filmUtilities.jl
 """
 
@@ -31,6 +32,8 @@ function blkFD(arg::Float64)::Float64
 	end
 end
 precompile(blkFD, (Float64, ))
+
+
 """
 	
 	siRspBck(enr::Float64, tmp::Float64)::ComplexF64
@@ -43,9 +46,86 @@ negligible in comparison to free carrier contributions.
 # reflectance measurements. Journal of thermophysics and heat transfer, 19(4), 558-565. 
 @inline function siRspBck(enr::Float64, tmp::Float64)::ComplexF64
 	#Gavin: Changed T -> T - 273.15  #equation from reference says T is in celsius 
-	return ^(sqrt(4.565 + /(97.3, ^(3.648,2) - ^(/(enr * 1.24, muEv), 2))) + (-1.864 * ^(10, -4) + 5.394 * /(^(10, -3), ^(3.648,2) - ^(/(enr * 1.24, muEv), 2))) * (tmp - 273.15), 2) - /(^(hBEv * 1.0e13, 2), enr * (enr + im * 1.0e10 * hBEv))
+	#Timans
+	kIB = SialphaIB(enr, tmp) * muEv / enr * 1e-4 / (4.0 * pi )   #multiplying by 1e-4 to convert from um to cm
+	#J-M
+	return ^(sqrt(4.565 + /(97.3, ^(3.648,2) - ^(/(enr * 1.24, muEv), 2))) + (-1.864 * ^(10, -4) + 5.394 * /(^(10, -3), ^(3.648,2) - ^(/(enr * 1.24, muEv), 2))) * (tmp - 273.15) + im*kIB, 2) - /(^(hBEv * 1.0e13, 2), enr * (enr + im * 1.0e10 * hBEv)) 
+	#=Li
+	lm = muEv / enr #lmabda in um
+	T = tmp - 273.15	#from K to C
+	eps_r = 11.631 + 1.0268e-3 * T + 1.0384e-6 * T^2.0 - 8.1347e-10 * T^3.0
+	g = 1.0204 + 4.8011e-4 * T + 7.3835e-8 * T^2.0 
+	n = exp(1.786e-4 - 8.526e-6 * T - 4.685e-9 * T^2.0 + 1.363e-12 * T^3.0)
+	return eps_r + g*n/lm^2.0 =#
 end
 precompile(siRspBck, (Float64, Float64))
+
+"""
+	SikIB(enr::Float64,tmp::Float64)::Float64
+
+	Temperature dependent background permittivity of intrinsic silicon.  From Timans, Emissivity of silicon at elevated temperatures (1993)
+"""
+@inline function SialphaIB(enr::Float64, tmp::Float64)::Float64
+
+
+	blzk = 8.6173e-5
+	return 1.0/enr * (F1(enr-siEnrGap(tmp)+blzk*212.0)/(exp(212.0/tmp) - 1.0) + F1(enr-siEnrGap(tmp)-blzk*212.0)/(1.0 - exp(-212.0/tmp)) + F2(enr-siEnrGap(tmp)+blzk*670.0)/(exp(670.0/tmp) - 1.0) + F2(enr-siEnrGap(tmp)-blzk*670.0)/(1.0 - exp(-670.0/tmp)) ) + Sialpha3a(enr,tmp) + Sialpha4a(enr,tmp)
+end
+precompile(SialphaIB, (Float64, Float64 ))
+
+@inline function F1(x::Float64)::Float64
+
+	if x > 0.0055
+		
+		return 0.504*sqrt(x) + 392.0*(x - 0.0055)^2.0
+	elseif x > 0.0
+		 
+		return 0.504*sqrt(x)
+	else
+		return 0.0
+	end
+end
+precompile(F1, (Float64, ))
+
+@inline function F2(x::Float64)::Float64
+
+	if x > 0.0055
+		
+		return 18.08*sqrt(x) + 5760.0*(x - 0.0055)^2.0
+	elseif x > 0.0
+		 
+		return 18.08*sqrt(x)
+	else
+		return 0.0
+	end
+end
+precompile(F2, (Float64, ))
+
+@inline function Sialpha3a(enr::Float64, tmp::Float64)::Float64
+	#alpha 3a
+
+	return Sialphaia(enr,tmp,536.0,1050.0)
+end
+precompile(Sialpha3a, (Float64, Float64 ))
+
+@inline function Sialpha4a(enr::Float64, tmp::Float64)::Float64
+	#alpha 4a
+
+	return Sialphaia(enr,tmp,988.0,1420.0)
+end
+precompile(Sialpha4a, (Float64, Float64 ))
+
+@inline function Sialphaia(enr::Float64, tmp::Float64, coef::Float64, Tcoeff::Float64)::Float64
+	#alpha for 3a and 4a
+
+
+	if enr < siEnrGap(tmp) - 8.6173e-5 * Tcoeff
+		return 0.0
+	else
+		return coef*(enr - siEnrGap(tmp) + 8.6173e-5*Tcoeff)^2.0 / (enr*(exp(Tcoeff/tmp - 1.0)))
+	end
+end
+precompile(Sialphaia, (Float64, Float64, Float64, Float64 ))
 """
 
 	siEnrGap(tmp::Float64)::Float64 
@@ -114,7 +194,7 @@ Used as an implicit equation for determining the correct Fermi energy.
 """
 @inline function frmZero(enrFrm::Float64, tmp::Float64, dnr::dptDsc, acp::dptDsc)::Float64
 
-	return itrnV(tmp) * blkFD(-/(enrFrm, blzK * tmp)) + /(dnr.ccn, 1.0 + 2.0 * exp(/(enrFrm - siEnrGap(tmp) + dnr.enr, blzK * tmp))) - itrnC(tmp) * blkFD(/(enrFrm - siEnrGap(tmp), blzK * tmp)) - /(acp.ccn, 1.0 + 4.0 * exp(/(acp.enr - enrFrm, blzK * tmp)))
+	return itrnV(tmp) * blkFD(-/(enrFrm, blzK * tmp)) + /(dnr.ccn, 1.0 + 2.0 * exp(/(enrFrm - dnr.enr, blzK * tmp))) - itrnC(tmp) * blkFD(/(enrFrm - siEnrGap(tmp), blzK * tmp)) - /(acp.ccn, 1.0 + 4.0 * exp(/(acp.enr - enrFrm, blzK * tmp)))
 end
 precompile(frmZero, (Float64, Float64, dptDsc, dptDsc))
 """
@@ -126,40 +206,40 @@ Computation of model parameters for doped silicon, see preamble of source file f
 function prmMSi(tmp::Float64, dnr::dptDsc, acp::dptDsc)::siDsc
 
 	# Self-consistent Fermi level.
-	frmFnc(flvl) = frmZero(flvl, tmp, dnr, acp)
-	enrFrm = find_zero(frmFnc, /(siEnrGap(tmp), 2.0))
+	frmFnc(flvl) = frmZero(flvl, tmp, dnr, acp)					#verified
+	enrFrm = find_zero(frmFnc, /(siEnrGap(tmp), 2.0))			#verified
 
-	# Number of ionized acceptor and donor dopants (carriers), units cm^-3. #verified
-	cncDnr = dnrCcn(enrFrm, tmp, dnr)
-	cncAcp = acpCcn(enrFrm, tmp, acp)
+	# Number of ionized acceptor and donor dopants (carriers), units cm^-3. 
+	cncDnr = dnrCcn(enrFrm, tmp, dnr)							#verified
+	cncAcp = acpCcn(enrFrm, tmp, acp)							#verified
 
 	# Room temperature total electron scattering time.
-	stETR = (141.0 + /(19.5, 1.0 + ^(/(dnr.ccn, 1.3 * ^(10.0, 17.0)), 0.91))) * 1.0e-15
+	stETR = (141.0 + /(19.5, 1.0 + ^(/(dnr.ccn, 1.3 * ^(10.0, 17.0)), 0.91))) * 1.0e-15		#verified
 	# Room temperature total hole scattering time.
-	stHTR = (10.0 + /(94.0, 1.0 + ^(/(acp.ccn, 1.9 * ^(10.0, 17.0)), 0.76))) * 1.0e-15
+	stHTR = (10.0 + /(94.0, 1.0 + ^(/(acp.ccn, 1.9 * ^(10.0, 17.0)), 0.76))) * 1.0e-15		#verified
 
 	# Room temperature electron-lattice scattering time.
-	stELR = 2.23 * 1.0e-13
+	stELR = 2.23 * 1.0e-13											#verified
 	# Room temperature hole-lattice scattering time.
-	stHLR = 1.06 * 1.0e-13
+	stHLR = 1.06 * 1.0e-13											#verified
 	# Room temperature electron-impurity scattering time. 
-	stEIR = /(stETR * stELR, stELR - stETR) 
+	stEIR = /(stETR * stELR, stELR - stETR) 						#verified
 	# Room temperature hole-impurity scattering time. 
-	stHIR = /(stHTR * stHLR, stHLR - stHTR) 
+	stHIR = /(stHTR * stHLR, stHLR - stHTR) 						#verified
 
 	# Include temperature dependence scattering times.
 	# Electrons 
-	stEI = stEIR * ^(/(tmp, 300), 1.5)
-	stEL = stELR * ^(/(tmp, 300), -3.8)
+	stEI = stEIR * ^(/(tmp, 300), 1.5)								#verified
+	stEL = stELR * ^(/(tmp, 300), -3.8)								#verified
 	# Holes
-	stHI = stHIR * ^(/(tmp, 300), 1.5)
-	stHL = stHLR * ^(/(tmp, 300), -3.6)
+	stHI = stHIR * ^(/(tmp, 300), 1.5)								#verified
+	stHL = stHLR * ^(/(tmp, 300), -3.6)								#verified
 
 	# Total donor and acceptor decay rates.  
 	dnrDcy = hBEv * (/(1.0, stEI) + /(1.0, stEL))
 	acpDcy = hBEv * (/(1.0, stHI) + /(1.0, stHL))
 
-	# Electron and hole effective masses.
+	# Electron and hole effective masses. #verified
 	mEffE = 0.27 
 	mEffH = 0.37
 
